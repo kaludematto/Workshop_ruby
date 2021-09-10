fb495aa38a7ca120b1b3de5632525b86d4c61e6b
kaludematto <kalu.dematto@gmail.com>
2021-09-09 22:04:59 -0300

    Atualizacao do Botao select e alggumas outras funcionalidades

--- app/controllers/exame_medicos_controller.rb
+++ app/controllers/exame_medicos_controller.rb
@@ -58,6 +58,7 @@ class ExameMedicosController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    
     def set_exame_medico
       @exame_medico = ExameMedico.find(params[:id])
     end
