d9f5370ea023014f7aa776867f937335bef38f5d
kaludematto <kalu.dematto@gmail.com>
2021-09-16 07:31:19 -0300

    testando

--- app/models/exame_medico.rb
+++ app/models/exame_medico.rb
@@ -1,5 +1,6 @@
 class ExameMedico < ApplicationRecord
   belongs_to :laboratorio
+  
   validates :nome, :presence => true
   validates :tipo, :presence => true
   validates :status,:presence => true
--- app/models/laboratorio.rb
+++ app/models/laboratorio.rb
@@ -1,5 +1,6 @@
 class Laboratorio < ApplicationRecord
     has_many :exame_medico, dependent: :destroy
+    
     validates :nome, :presence => true
     validates :endereco, :presence => true
     validates :status,:presence => true
